22d91fdd0bc5ec3140122caa7f987f24238255ca
Omer Geva <ogeva97@gmail.com>
2020-05-21 11:45:46 +0300

    can book

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,4 +1,22 @@
 class BookingsController < ApplicationController
+  def index
+    @bookings = Booking.all
+    Date.beginning_of_week = :sunday
+  end
   def create
+    booking = Booking.new(booking_params)
+    haircut = Haircut.find_by(title: params[:booking][:haircut])
+    booking.haircut = haircut
+
+    if booking.save
+      redirect_to root_path
+    else
+      render 'haircuts/index'
+    end
+  end
+
+  private
+  def booking_params
+    params.require(:booking).permit(:client_name, :client_email, :phone_number, :message,:date, :time)
   end
 end
--- app/models/haircut.rb
+++ app/models/haircut.rb
@@ -2,4 +2,6 @@ class Haircut < ApplicationRecord
   belongs_to :user
   has_many :bookings
 
+
+  HAIRCUT_TYPES = ["Haircut", "Beard Trim", "Haircut & Beard"]
 end
